e1a5e71503d922095333ae43c2520ebb3a10f835
Mehar Johal <36781287+mjohal3@users.noreply.github.com>
2023-10-23 19:10:25 -0400

    Add bat grunt, rock frog grunt, rock grunt, warden, cutscene, and attacking ability/health for enemies

--- Assets/Scripts/ThirdPController.cs
+++ Assets/Scripts/ThirdPController.cs
@@ -197,9 +197,11 @@ public class ThirdPController : MonoBehaviour
             tookDamage = false;
             damageAudio.Play();
         } else {
-            AnimatorStateInfo damageStateInfo = animator.GetCurrentAnimatorStateInfo(2); 
+            AnimatorStateInfo damageStateInfo = animator.GetCurrentAnimatorStateInfo(2);
             // Check if the damage animation is done playing
+            Debug.Log("Else");
             if (damageStateInfo.IsName("TakeDamage") && damageStateInfo.normalizedTime >= 1 && !animator.IsInTransition(2)) {
+                Debug.Log("Inner Else");
                 animator.SetBool("tookDamage", false);
                 tookDamage = false;
                 animator.SetLayerWeight(2, 0);
