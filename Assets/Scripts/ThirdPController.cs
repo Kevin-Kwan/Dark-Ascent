485b3690be72d37541a00fad149ac028ea9902c1
Kevin Kwan <kkwan9@gatech.edu>
2023-11-17 13:29:30 -0500

    attempt at floaty jump fix (#60)

--- Assets/Scripts/ThirdPController.cs
+++ Assets/Scripts/ThirdPController.cs
@@ -67,6 +67,7 @@ public class ThirdPController : MonoBehaviour
     public float jumpHeight = 1.0f;
     public float jumpAdjustment = -2.0f;
     public float gravity = -9.81f;
+    public float gravityFactor = 2.0f;
     private int jumpCount = 0;
     public int maxJumps = 2;
     public Vector3 playerVelocity;
@@ -426,7 +427,7 @@ public class ThirdPController : MonoBehaviour
         currentVelY = Mathf.Lerp(currentVelY, targetVelY, speedChangeRate);
         animator.SetFloat("velY", currentVelY);
 
-        playerVelocity.y += Physics.gravity.y * Time.deltaTime;
+        playerVelocity.y += gravityFactor * gravity * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
     }
 
@@ -517,7 +518,7 @@ public class ThirdPController : MonoBehaviour
         if (playerVelocity.y < 0) {
             playerVelocity.y = 0f;
         }
-        playerVelocity.y += Mathf.Sqrt(jumpHeight * jumpAdjustment * gravity);
+        playerVelocity.y += Mathf.Sqrt(jumpHeight * jumpAdjustment * gravityFactor * gravity);
         animator.SetBool("isJumping", true);
         jumpAudio.Play();
     }
