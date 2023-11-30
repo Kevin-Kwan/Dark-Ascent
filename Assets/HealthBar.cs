c264854ef520682cf1beaae694343003d2cf874a
Amal <achaudry6@gatech.edu>
2023-11-30 14:17:19 -0500

    changing colors

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -6,6 +6,13 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
+    public Image fill;
+    private Color fullHealthColor; // Store the full health color
+
+    private void Start()
+    {
+        fullHealthColor = new Color(0f, 1f, 0f, 1f); // Store the original color when the health bar initializes
+    }
 
     public void SetMaxHealth(float health) {
         slider.maxValue = health;
@@ -14,5 +21,14 @@ public class HealthBar : MonoBehaviour
 
     public void SetHealth(float health) {
         slider.value = health;
+        if (health <= 100) {
+            fill.color = fullHealthColor;
+        }
+        if (health <= 50) {
+            fill.color = new Color(1f, 0.92f, 0.016f, 1f);
+        }
+        if (health <= 20) {
+            fill.color = new Color(1f, 0f, 0f, 1f); 
+        }
     }
 }
